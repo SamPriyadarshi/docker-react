8e9fa5076892fdff061e8f2f896b09b9ca444e95
Samrat Priyadarshi <samratpriyadarshi46@outlook.com>
2018-11-17 13:29:42 +0530

    changed content of app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This is Samrat Priyadarshi. The Cloud and Kubernetes Ninja.
           </p>
           <a
             className="App-link"
